dfd0c47f0caeef85fe997916fa86e794c7ee0f5a
Jim Orcheson <jimorcheson@gmail.com>
2025-09-21 11:09:55 -0400

    Use FileChooser to select CSV file
    Print selected file name or "cancelled".

--- src/flexishowbuilder/BuilderGUI.java
+++ src/flexishowbuilder/BuilderGUI.java
@@ -1,6 +1,14 @@
 package flexishowbuilder;
 
+import java.io.File;
+import java.util.List;
+
+import javax.swing.filechooser.FileSystemView;
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,12 +17,20 @@ import javafx.scene.layout.StackPane;
 public class BuilderGUI extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        StackPane pane = new StackPane();
-        pane.getChildren().add(new Label("Hello, FlexiShow Builder!"));
-        Scene scene = new Scene(pane, 400, 300);
-        stage.setScene(scene);
-        stage.setTitle("FlexiShow Builder");
-        stage.show();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select CSV File");
+        fileChooser.setInitialDirectory(FileSystemView.getFileSystemView().getHomeDirectory());
+        Stage savedStage = stage;
+        fileChooser.getExtensionFilters().add(new ExtensionFilter("CSV Files", "*.csv"));
+
+        File csv = fileChooser.showOpenDialog(savedStage);
+
+        if (csv != null) {
+            System.out.println("CSV File selected " + csv.getName() + "..");
+        } else {
+            System.out.println("CSV file selection cancelled.");
+        }
+        Platform.exit();
     }
 
     public static void main(String[] args) {
