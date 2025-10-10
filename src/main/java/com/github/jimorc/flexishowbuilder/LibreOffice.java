589636b159f79943c6b7a04ba676912ab3ec0334
Jim Orcheson <jimorcheson@gmail.com>
2025-10-10 10:37:57 -0400

    Add XLSAppFactory and LibreOffice classes
    XLSAppFactory only checks if LibreOffice is installed, then returns a LibreOffice object.
    LibreOffiice doesn't really do anything yet.

---
+++ src/main/java/com/github/jimorc/flexishowbuilder/LibreOffice.java
@@ -0,0 +1,8 @@
+package com.github.jimorc.flexishowbuilder;
+
+public class LibreOffice extends XLSApp {
+     @Override
+    public void createXLS(OutputCSV cvs) throws Exception {
+        // TODO: add implementation.
+    }
+}
