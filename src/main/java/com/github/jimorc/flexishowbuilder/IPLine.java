a92bb410c59a038990b1a42b2d29dbf87d15fde0
Jim Orcheson <jimorcheson@gmail.com>
2025-11-10 08:04:46 -0500

    Move setting widths to IPLine from IPLines

--- src/main/java/com/github/jimorc/flexishowbuilder/IPLine.java
+++ src/main/java/com/github/jimorc/flexishowbuilder/IPLine.java
@@ -6,6 +6,14 @@ import javafx.scene.layout.HBox;
  * IPLine is an HBox that displays a CSVLine as IPLFields.
  */
 public class IPLine extends HBox {
+    // Used in switch cases to identify columns.
+    // Cases must be constants, so can't use IPLColumn.ordinal().
+    private final int imageColumn = 0;              // same as IPLColumn.IMAGE_FILE_NAME.ordinal();
+    private final int titleColumn = 1;              // same as IPLColumn.IMAGE_TITLE.ordinal();
+    private final int personFullNameColumn = 2;     // same as IPLColumn.PERSON_FULL_NAME.ordinal();
+    private final int personFirstNameColumn = 3;    // same as IPLColumn.PERSON_FIRST_NAME.ordinal();
+    private final int personLastNameColumn = 4;     // same as IPLColumn.PERSON_LAST_NAME.ordinal();
+
     private CSVLine line;
 
     /**
@@ -13,14 +21,6 @@ public class IPLine extends HBox {
      * @param line the CSVLine to display.
      */
     public IPLine(CSVLine line, IPLFieldWidths fieldWidths) {
-        // Used in switch cases to identify columns.
-        // Cases must be constants, so can't use IPLColumn.ordinal().
-        final int imageColumn = 0;              // same as IPLColumn.IMAGE_FILE_NAME.ordinal();
-        final int titleColumn = 1;              // same as IPLColumn.IMAGE_TITLE.ordinal();
-        final int personFullNameColumn = 2;     // same as IPLColumn.PERSON_FULL_NAME.ordinal();
-        final int personFirstNameColumn = 3;    // same as IPLColumn.PERSON_FIRST_NAME.ordinal();
-        final int personLastNameColumn = 4;     // same as IPLColumn.PERSON_LAST_NAME.ordinal();
-
         super();
         this.line = line;
         final int spacing = 5;
@@ -43,6 +43,36 @@ public class IPLine extends HBox {
                     break;
                 case personLastNameColumn:
                     fieldWidths.setMaxPersonLastNameWidth(ipF.getFieldWidth());
+                    break;
+                default:
+                    // handle more than 5 columns
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Sets the field widths of the IPLFields in this IPLine.
+     * @param fieldWidths the IPLFieldWidths object containing the widths to set.
+     */
+    public void setFieldWidths(IPLFieldWidths fieldWidths) {
+        for (int col = 0; col < getChildren().size(); col++) {
+            IPLField ipField = (IPLField) getChildren().get(col);
+            switch (col) {
+                case imageColumn:
+                    ipField.setFieldWidth(fieldWidths.getImageFileNameWidth());
+                    break;
+                case titleColumn:
+                    ipField.setFieldWidth(fieldWidths.getImageTitleWidth());
+                    break;
+                case personFullNameColumn:
+                    ipField.setFieldWidth(fieldWidths.getPersonFullNameWidth());
+                    break;
+                case personFirstNameColumn:
+                    ipField.setFieldWidth(fieldWidths.getPersonFirstNameWidth());
+                    break;
+                case personLastNameColumn:
+                    ipField.setFieldWidth(fieldWidths.getPersonLastNameWidth());
                     break;
                 default:
                     // handle more than 5 columns
--- src/main/java/com/github/jimorc/flexishowbuilder/IPLines.java
+++ src/main/java/com/github/jimorc/flexishowbuilder/IPLines.java
@@ -16,14 +16,6 @@ public class IPLines extends VBox {
      * @param fieldWidths the IPLFieldWidths object to update.
      */
     public IPLines(InputCSV csv, IPLFieldWidths fieldWidths) {
-        // Used in switch cases to identify columns.
-        // Cases must be constants, so can't use IPLColumn.ordinal().
-        final int imageColumn = 0;              // same as IPLColumn.IMAGE_FILE_NAME.ordinal();
-        final int titleColumn = 1;              // same as IPLColumn.IMAGE_TITLE.ordinal();
-        final int personFullNameColumn = 2;     // same as IPLColumn.PERSON_FULL_NAME.ordinal();
-        final int personFirstNameColumn = 3;    // same as IPLColumn.PERSON_FIRST_NAME.ordinal();
-        final int personLastNameColumn = 4;     // same as IPLColumn.PERSON_LAST_NAME.ordinal();
-
         super();
         setSpacing(SPACING);
         setPadding(PADDING);
@@ -34,29 +26,7 @@ public class IPLines extends VBox {
         }
         for (int i = 0; i < getChildren().size(); i++) {
             IPLine ipLine = (IPLine) getChildren().get(i);
-            for (int col = 0; col < ipLine.getChildren().size(); col++) {
-                IPLField ipField = (IPLField) ipLine.getChildren().get(col);
-                switch (col) {
-                    case imageColumn:
-                        ipField.setFieldWidth(fieldWidths.getImageFileNameWidth());
-                        break;
-                    case titleColumn:
-                        ipField.setFieldWidth(fieldWidths.getImageTitleWidth());
-                        break;
-                    case personFullNameColumn:
-                        ipField.setFieldWidth(fieldWidths.getPersonFullNameWidth());
-                        break;
-                    case personFirstNameColumn:
-                        ipField.setFieldWidth(fieldWidths.getPersonFirstNameWidth());
-                        break;
-                    case personLastNameColumn:
-                        ipField.setFieldWidth(fieldWidths.getPersonLastNameWidth());
-                        break;
-                    default:
-                        // handle more than 5 columns
-                        break;
-                }
-            }
+            ipLine.setFieldWidths(fieldWidths);
         }
     }
 }
