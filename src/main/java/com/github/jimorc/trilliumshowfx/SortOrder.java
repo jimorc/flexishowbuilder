268ba98652dd9e30f41820ffca12f07d5014ea92
Jim Orcheson <jimorcheson@gmail.com>
2026-06-26 08:37:59 -0400

    Fix sort order button tooltips
    Remove person slide text from sortOrderLabel text for each SortOrder.
    This text will be added as determined by genPersonSlides checkbox setting.
    Person slides cannot be created for DontSort sortOrder.

--- src/main/java/com/github/jimorc/trilliumshowfx/SortOrder.java
+++ src/main/java/com/github/jimorc/trilliumshowfx/SortOrder.java
@@ -7,45 +7,40 @@ public enum SortOrder {
     /**
      * Don't sort - use the image order in the CSV file.
      */
-    DontSort("Don't Sort", "Do not sort - use the order in the CSV file.",
-        "Slides remain in the order they appear in the CSV file.\n"
-        + "No person slides will be created."),
+    DontSort("Don't Sort", "Do not sort - use the order in the CSV file.\n"
+        + "No person slides will be created.",
+        "Slides remain in the order they appear in the CSV file."),
     /**
      * As Is - sort by person in the order they first appear in the CSV file.
      */
     AsIs("As Is",
         "No sorting - use the order in the CSV file. All images for each person are grouped together.",
         "Slides will be sorted by person in the order they first appear in the CSV file.\n"
-        + "All images for each person are grouped together.\n"
-        + "Person slides will be created for each person."),
+        + "All images for each person are grouped together"),
     /**
      * Alphabetical by Full Name.
      */
     AlphabeticalByFullName("Alphabetical by Full Name",
         "Sort in alphabetical order by full name.",
-        "Slides will be sorted in alphabetical order by full name.\n"
-        + "Person slides will be created for each person."),
+        "Slides will be sorted in alphabetical order by full name."),
     /**
      * Alphabetical by Last Name then First Name.
      */
     AlphabeticalByLastNameThenFirstName("Alphabetical by Last Name then First Name",
         "Sort in alphabetical order by last name then first name.",
-        "Slides will be sorted in alphabetical order by last name then first name.\n"
-        + "Person slides will be created for each person."),
+        "Slides will be sorted in alphabetical order by last name then first name."),
     /**
      * Alphabetical by Full Name (Reverse).
      */
     AlphabeticalByFullNameReverse("Alphabetical by Full Name (Reverse)",
         "Sort in reverse alphabetical order by full name.",
-        "Slides will be sorted in reverse alphabetical order by full name.\n"
-        + "Person slides will be created for each person."),
+        "Slides will be sorted in reverse alphabetical order by full name."),
     /**
      * Alphabetical by Last Name then First Name (Reverse).
      */
     AlphabeticalByLastNameThenFirstNameReverse("Alphabetical by Last Name then First Name (Reverse)",
         "Sort in reverse alphabetical order by last name then first name.",
-        "Slides will be sorted in reverse alphabetical order by last name then first name.\n"
-        + "Person slides will be created for each person.");
+        "Slides will be sorted in reverse alphabetical order by last name then first name.");
 
     private String buttonLabel;
     private String tooltipText;
